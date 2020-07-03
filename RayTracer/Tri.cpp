c8af2004f60a8c90abeb8bcbc0e8dd95bf246676
Margot <gotgot07@hotmail.fr>
2020-07-04 01:18:06 +0200

    Tri normale bug

--- RayTracer/Tri.cpp
+++ RayTracer/Tri.cpp
@@ -1,27 +1,62 @@
 #include "Tri.h"
+#include "vec3.h"
 
-Tri::Tri(const vec3& pos, float r, Material* mat, vec3 color)
+Tri::Tri(float r, const vec3& pos, Material* mat, vec3 color)
 	: radius(r), Primitive::Primitive(pos, mat, color)
 {
 }
 
 float Tri::intersect(const ray& ray) const
 {
-	vec3 diff = ray.origin - position;
-	float a = diff.dot(diff);
-	vec3 v = ray.direction;
-	float b = diff.dot(v.normalize());
-	float c = a - radius * radius;
-	float t = b * b - c;
+	float t = 1;
 
-	if (t >= 0.f)
-	{
-		t = -b - sqrtf(t);
-	}
+    vec3 v0 = vec3{ position.x, position.y + radius/4, position.z + 5};
+    vec3 v1 = vec3{ position.x - radius / 4, position.y - radius / 4,  position.z - 1 };
+    vec3 v2 = vec3{ position.x + radius/4, position.y - radius/4,  position.z + 6 };
+
+	vec3 v0v1 = v1 - v0;
+	vec3 v0v2 = v2 - v0;
+	vec3 N = v0v1^(v0v2);
+
+	float NdotRayDirection = N.dot(ray.direction);
+	if (fabs(NdotRayDirection) < 0.0001)
+		return 0; 
+
+	float d = N.dot(v0);
+	t = (N.dot(ray.origin) + d) / NdotRayDirection;
+	if (t < 0) t = 0;
+
+    vec3 P = ray.origin + ray.direction * t;
+    vec3 C;
+
+    vec3 edge0 = v1 - v0;
+    vec3 vp0 = P - v0;
+    C = edge0^(vp0);
+    if (N.dot(C) < 0) t = 0; 
+
+    // edge 1
+    vec3 edge1 = v2 - v1;
+    vec3 vp1 = P - v1;
+    C = edge1^(vp1);
+    if (N.dot(C) < 0)  t = 0;
+
+    // edge 2
+    vec3 edge2 = v0 - v2;
+    vec3 vp2 = P - v2;
+    C = edge2^(vp2);
+    if (N.dot(C) < 0) t = 0; 
+
 	return t;
 }
 
 vec3 Tri::calculateNormal(const vec3& p) const
 {
-	return (p - position).normalize();
+    vec3 v0 = vec3{ position.x, position.y + radius / 4,  position.z + 5 };
+    vec3 v1 = vec3{ position.x - radius / 4, position.y - radius / 4,  position.z - 1};
+    vec3 v2 = vec3{ position.x + radius / 4, position.y - radius / 4,  position.z + 6 };
+
+    vec3 v0v1 = v1 - v0;
+    vec3 v0v2 = v2 - v0;
+
+	return v0v2^v0v1;
 }
--- RayTracer/Tri.h
+++ RayTracer/Tri.h
@@ -4,11 +4,12 @@
 
 class Tri : public Primitive
 {
+private:
 	float radius;
 
 public:
 	//constructeur
-	Tri(const vec3& pos, float r, Material* mat, vec3 color);
+	Tri(float r, const vec3& pos, Material* mat, vec3 color);
 
 	float intersect(const ray& ray) const;
 	virtual vec3 calculateNormal(const vec3& p) const final;
